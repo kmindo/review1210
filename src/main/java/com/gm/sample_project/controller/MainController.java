32d42f96852804d561b3dd4e7e5b2db0e7e2bed9
dgmin <dgmin9440@gmail.com>
2021-12-10 10:13:34 +0900

    포트 변경 및 메인 컨트롤러 추가

---
+++ src/main/java/com/gm/sample_project/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.gm.sample_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String getMain(){
+        return "/index";
+    }
+    
+}
